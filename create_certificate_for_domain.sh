b5fad116f990dfbdc8af72c0ef280947dbfe04c6
Maxim Kovtun <mkovtun@smartru.com>
2017-10-12 16:55:14 +0300

    Common Name should't contain wildcard

--- create_certificate_for_domain.sh
+++ create_certificate_for_domain.sh
@@ -17,7 +17,7 @@ fi
 DOMAIN=$1
 COMMON_NAME=${2:-$1}
 
-SUBJECT="/C=CA/ST=None/L=NB/O=None/CN=*.$COMMON_NAME"
+SUBJECT="/C=CA/ST=None/L=NB/O=None/CN=$COMMON_NAME"
 NUM_OF_DAYS=999
 openssl req -new -newkey rsa:2048 -sha256 -nodes $KEY_OPT device.key -subj "$SUBJECT" -out device.csr
 cat v3.ext | sed s/%%DOMAIN%%/$COMMON_NAME/g > /tmp/__v3.ext
